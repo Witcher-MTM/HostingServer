1ca85c79c2080c5e4453b27e1982d01228639ec6
Witcher-MTM <phantomit228@gmail.com>
2023-05-15 19:03:32 +0300

    Update Auth.controller.js

--- controller/Auth.controller.js
+++ controller/Auth.controller.js
@@ -15,29 +15,17 @@ class AuthController {
     console.log(logger.regexSymbol('-', 50), "info")
   }
   async authenticateUser(req, res) {
-    console.log(logger.regexSymbol('-', 50), "info")
-    console.log(`start authenticateUser at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
     try {
       const user = await UserController.getUserByEmail(req, res, true);
       const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
       if (isPasswordMatch) {
-        console.log(`send info at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
-        console.log(`end authenticateUser at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
-        console.log(logger.regexSymbol('-', 50), "info")
-        return res.status(200).send('true');
+        return res.status(200).send(true);
       } else {
-        console.log(`send info at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
-        console.log(`end authenticateUser at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
-        console.log(logger.regexSymbol('-', 50), "info")
         return res.status(401).send('Incorrect password');
       }
     } catch (err) {
-      console.log(`send error message: ${err.message} at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "error")
-      console.log(`end authenticateUser at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
-      console.log(logger.regexSymbol('-', 50), "info")
       return res.status(500).send(err.message);
     }
-
   }
 }
 
