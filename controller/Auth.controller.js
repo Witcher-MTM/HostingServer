201d328de1964ac0aeccd8838d7b6e41c1ec5bde
Witcher-MTM <phantomit228@gmail.com>
2023-05-15 16:48:43 +0300

    add logger to category

--- controller/Auth.controller.js
+++ controller/Auth.controller.js
@@ -1,26 +1,42 @@
 const UserController = require('./user.controller')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken');
-class AuthController{
-    async register(req,res){
-        const user = await UserController.addUser(req,res,true)
-        const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-
-        res.status(200).json({ token });
-    }
-    async authenticateUser(req, res) {
-        try {
-          const user = await UserController.getUserByEmail(req, res, true);
-          const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
-          if (isPasswordMatch) {
-            return res.status(200).send('true');
-          } else {
-            return res.status(401).send('Incorrect password');
-          }
-        } catch (err) {
-          return res.status(500).send(err.message);
-        }
+class AuthController {
+  async register(req, res) {
+    console.log(logger.regexSymbol('-', 50), "info")
+    console.log(`start register at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+    const user = await UserController.addUser(req, res, true)
+    const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+    console.log(`send info at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+    res.status(200).json({ token });
+    console.log(`end register at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+    console.log(logger.regexSymbol('-', 50), "info")
+  }
+  async authenticateUser(req, res) {
+    console.log(logger.regexSymbol('-', 50), "info")
+    console.log(`start authenticateUser at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+    try {
+      const user = await UserController.getUserByEmail(req, res, true);
+      const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+      if (isPasswordMatch) {
+        console.log(`send info at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(`end authenticateUser at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(logger.regexSymbol('-', 50), "info")
+        return res.status(200).send('true');
+      } else {
+        console.log(`send info at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(`end authenticateUser at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(logger.regexSymbol('-', 50), "info")
+        return res.status(401).send('Incorrect password');
       }
+    } catch (err) {
+      console.log(`send error message: ${err.message} at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "error")
+      console.log(`end authenticateUser at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+      console.log(logger.regexSymbol('-', 50), "info")
+      return res.status(500).send(err.message);
+    }
+
+  }
 }
 
 module.exports = new AuthController()
--- controller/category.controller.js
+++ controller/category.controller.js
@@ -4,42 +4,49 @@ const { Op } = require('sequelize');
 const FormattedDate = require('../module/FormattedDate')
 class CategoryController {
     async getCategories(req, res, local) {
-        try {
-            const result = await Category.findAll({
-                where: {
-                    image_link: {
-                        [Op.not]: "tmp"
-                    }
+        console.log(logger.regexSymbol('-', 50), "info")
+        console.log(`start getCategories at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        const result = await Category.findAll({
+            where: {
+                image_link: {
+                    [Op.not]: "tmp"
                 }
-            });
+            }
+        }).then((result) => {
             if (local) {
                 return result;
             } else {
                 return res.status(200).send(result);
             }
-        } catch (err) {
+        }).catch((err) => {
             return res.status(400).send(err.message);
-        }
+        })
+        console.log(`end getCategories at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(logger.regexSymbol('-', 50), "info")
     }
     async addCategory(req, res) {
-       
+        console.log(logger.regexSymbol('-', 50), "info")
+        console.log(`start addCategory at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
         const { user_id, name, image_link, image_color, color, lastUsed, isIncome } = req.body
-        try {
-            const result = await Category.create({
-                user_id: user_id,
-                name: name,
-                image_link: image_link,
-                image_color: image_color,
-                color: color,
-                lastUsed: lastUsed,
-                isIncome: isIncome
-            })
+        const result = await Category.create({
+            user_id: user_id,
+            name: name,
+            image_link: image_link,
+            image_color: image_color,
+            color: color,
+            lastUsed: lastUsed,
+            isIncome: isIncome
+        }).then((result) => {
             return res.status(200).send(result)
-        } catch (err) {
+        }).catch((err) => {
             return res.status(400).send(err.message)
-        }
+        })
+        console.log(`end addCategory at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(logger.regexSymbol('-', 50), "info")
     }
     async deleteCategoryByID(req, res) {
+        console.log(logger.regexSymbol('-', 50), "info")
+        console.log(`start deleteCategoryByID at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
         await Category.destroy({
             where: {
                 id: req.params.category_id,
@@ -62,9 +69,12 @@ class CategoryController {
             .catch((err) => {
                 return res.status(400).send(err.message)
             })
+        console.log(`end deleteCategoryByID at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(logger.regexSymbol('-', 50), "info")
     }
     async patchCategoryByID(req, res) {
-
+        console.log(logger.regexSymbol('-', 50), "info")
+        console.log(`start patchCategoryByID at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
         await Category.findOne({
             where: {
                 id: req.params.category_id,
@@ -99,8 +109,12 @@ class CategoryController {
             .catch((err) => {
                 return res.status(400).send(err.message)
             })
+        console.log(`end patchCategoryByID at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(logger.regexSymbol('-', 50), "info")
     }
     async getCategoryByUserID(req, res) {
+        console.log(logger.regexSymbol('-', 50), "info")
+        console.log(`start getCategoryByUserID at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
         await Category.findAll({
             where: {
                 user_id: req.params.user_id,
@@ -118,8 +132,12 @@ class CategoryController {
             .catch((err) => {
                 return res.status(400).send(err.message)
             })
+        console.log(`end getCategoryByUserID at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(logger.regexSymbol('-', 50), "info")
     }
     async getCategoryByID(req, res) {
+        console.log(logger.regexSymbol('-', 50), "info")
+        console.log(`start getCategoryByID at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
         await Category.findOne({
             where: {
                 id: req.params.category_id,
@@ -137,6 +155,8 @@ class CategoryController {
             .catch((err) => {
                 return res.status(400).send(err.message)
             })
+        console.log(`end getCategoryByID at ${FomrattedDate.toDateTimeSeconds(new Date())}`, "info")
+        console.log(logger.regexSymbol('-', 50), "info")
     }
 }
 
