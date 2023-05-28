c1ddbf7438e6c9d3c0a1ac783e7cf1a3ac8003ae
Witcher-MTM <phantomit228@gmail.com>
2023-05-28 19:23:54 +0300

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -27,7 +27,6 @@ class UserController {
                     return res.status(403).send("That email is taken")
                 }
             })
-            const apiKey = encodeURIComponent(bcrypt.hashSync("apikeyforserver"))
             const result = await User.create({
                 email: email,
                 isConfirmed: false,
